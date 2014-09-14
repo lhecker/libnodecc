a33623ade5790d5f9dffffa7cdff177d84eb3dcc
Leonard Hecker <leonard@hecker.io>
2014-09-14 10:24:59 +0200

    improved uv::loop
    added uv::loop::next_tick

--- include/libnodecc/uv/loop.h
+++ include/libnodecc/uv/loop.h
@@ -1,12 +1,18 @@
 #ifndef nodecc_loop_h
 #define nodecc_loop_h
 
+#include <functional>
+#include <vector>
 #include <uv.h>
+
 
 namespace uv {
 
 class loop {
 public:
+	typedef std::function<void()> on_tick_t;
+
+
 	explicit loop() noexcept;
 
 	~loop() noexcept;
@@ -15,13 +21,21 @@ public:
 	bool run_once();
 	bool run_nowait();
 
+	void stop();
+
 	bool alive();
+
+	void next_tick(const on_tick_t& cb);
+	void next_tick(on_tick_t&& cb);
+
 
 	operator uv_loop_t*();
 	operator const uv_loop_t*() const;
 
 protected:
 	uv_loop_t _loop;
+	uv_async_t _tick_async;
+	std::vector<on_tick_t> _tick_callbacks;
 };
 
 } // namespace uv
--- src/uv/loop.cc
+++ src/uv/loop.cc
@@ -1,10 +1,28 @@
 #include "libnodecc/uv/loop.h"
 
-#include <type_traits>
-
 
 uv::loop::loop() noexcept {
 	uv_loop_init(&this->_loop);
+	uv_async_init(&this->_loop, &this->_tick_async, [](uv_async_t* handle) {
+		auto self = reinterpret_cast<uv::loop*>(handle->data);
+
+		std::vector<on_tick_t> callbacks;
+		std::swap(callbacks, self->_tick_callbacks);
+
+		for (const on_tick_t& cb : callbacks) {
+			cb();
+
+			if (self->_loop.stop_flag) {
+				return;
+			}
+		}
+	});
+
+	uv_unref(reinterpret_cast<uv_handle_t*>(&this->_tick_async));
+
+	this->_loop.data = this;
+	this->_tick_async.data = this;
+	this->_tick_callbacks.reserve(8);
 }
 
 uv::loop::~loop() noexcept {
@@ -23,14 +41,27 @@ bool uv::loop::run_nowait() {
 	return 0 != uv_run(&this->_loop, UV_RUN_NOWAIT);
 }
 
+void uv::loop::stop() {
+	uv_stop(&this->_loop);
+}
+
+bool uv::loop::alive() {
+	return uv_loop_alive(&this->_loop);
+}
+
+void uv::loop::next_tick(const on_tick_t& cb) {
+	this->_tick_callbacks.emplace_back(cb);
+	uv_async_send(&this->_tick_async);
+}
+void uv::loop::next_tick(on_tick_t&& cb) {
+	this->_tick_callbacks.emplace_back(std::forward<on_tick_t>(cb));
+	uv_async_send(&this->_tick_async);
+}
+
 uv::loop::operator uv_loop_t*() {
 	return &this->_loop;
 }
 
 uv::loop::operator const uv_loop_t*() const {
 	return &this->_loop;
-}
-
-bool uv::loop::alive() {
-	return uv_loop_alive(&this->_loop);
 }
