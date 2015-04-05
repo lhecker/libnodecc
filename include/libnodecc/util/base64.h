41e4bc84b30d7d24c96db38258e28e7baed48a27
Leonard Hecker <leonard@hecker.io>
2015-04-05 20:01:12 +0200

    improved node::util::base64

--- include/libnodecc/util/base64.h
+++ include/libnodecc/util/base64.h
@@ -9,7 +9,16 @@ namespace util {
 
 class base64 {
 public:
-	static node::buffer encode(const node::buffer_view buffer);
+	/**
+	 * Encodes a buffer using base64 or base64url.
+	 */
+	static node::buffer encode(const node::buffer_view buffer, bool base64url = false);
+
+	/**
+	 * Decodes a base64 encoded buffer.
+	 *
+	 * WARNING: This decoder simply ignores invalid characters.
+	 */
 	static node::buffer decode(const node::buffer_view buffer);
 };
 
